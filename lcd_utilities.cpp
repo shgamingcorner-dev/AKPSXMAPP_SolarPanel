9bb6245df61350338ac6ce96d222648c1ac1a821
shgamingcorner-dev <shgamingcorner@gmail.com>
2026-08-01 18:07:05 +0800

    Fix pin allocations for LCD

--- lcd_utilities.cpp
+++ lcd_utilities.cpp
@@ -16,7 +16,7 @@ PortOut lcdPort(PortA, DISPLAY_LCD_MASK);
 DigitalOut LCD_RS(PC_7);   //  Register Select on LC
 DigitalOut LCD_EN(PA_12);   //  Enable on LCD controller
 //DigitalOut LCD_WR(PA_13);   //  Write on LCD controller
-DigitalOut LCD_WR(PB_12);   //  Write on LCD controller
+DigitalOut LCD_WR(PC_6);   //  Write on LCD controller
 
 void lcd_strobe(void);
 
